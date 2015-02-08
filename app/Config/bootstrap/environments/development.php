c8b5a8485ac5661c469d2391d2fd3fb79cb779c4
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2015-02-08 13:04:03 +0900

    CakePHP の土台を追加

---
+++ app/Config/bootstrap/environments/development.php
@@ -0,0 +1,10 @@
+<?php
+
+Environment::configure('development', true, [
+	'MYSQL_DB_HOST' => 'localhost',
+	'MYSQL_USERNAME' => 'root',
+	'MYSQL_PASSWORD' => 'password',
+	'MYSQL_DB_NAME' => 'blog',
+	'MYSQL_TEST_DB_NAME' => 'test_blog',
+	'MYSQL_PREFIX' => '',
+]);
