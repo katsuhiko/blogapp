4bf203f8e5aced3630088e511436dfb10075c9c2
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2015-02-10 15:52:01 +0900

    コメントを追加しました。

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -7,20 +7,34 @@ App::uses('AppController', 'Controller');
  */
 class PostsController extends AppController {
 
+	/**
+	 * Components
+	 *
+	 * @var array
+	 */
 	public $components = ['Paginator', 'RequestHandler', 'Session'];
 
+	/**
+	 * Index
+	 *
+	 * @return void
+	 */
 	public function index() {
 		$this->set('posts', $this->Paginator->paginate());
 	}
 
+	/**
+	 * Add
+	 *
+	 * @return void
+	 */
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Post->create($this->request->data);
 
 			if ($this->Post->save()) {
 				$this->Session->setFlash(__('新しい記事を受け付けました。'));
-				$this->redirect(['action' => 'index']);
-				return;
+				return $this->redirect(['action' => 'index']);
 			} else {
 				$this->Session->setFlash(__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'));
 			}
