3d3d2f78381628bc83d437b60058224590c0131f
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2015-02-10 15:41:57 +0900

    テストデータを Fabricate を使って生成するように変更しました。

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,20 +12,4 @@ class PostFixture extends CakeTestFixture {
 	 * @var array
 	 */
 	public $import = ['model' => 'Post'];
-
-	/**
-	 * Records
-	 *
-	 * @var array
-	 */
-	public $records = [
-		[
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-			'created' => '2015-02-10 05:49:48',
-			'modified' => '2015-02-10 05:49:48'
-		],
-	];
-
 }
